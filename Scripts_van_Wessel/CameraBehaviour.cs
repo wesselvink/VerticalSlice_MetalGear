40453f8762557ea12b2bbdf0057bdd4d2bf6cfb4
wesselvink <22092@ma-web.nl>
2017-12-22 23:34:16 +0100

    Scripts van Wessel

---
+++ Scripts_van_Wessel/CameraBehaviour.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBehaviour : MonoBehaviour
+{
+	public Vector3 startAngle, endAngle, currentAngle;
+	public Vector3 startPosition, endPosition;
+	public Transform[] views;
+	private Transform previousView;
+	public float transitionSpeed; 
+
+	float startTime, totalDistance;
+	public float duration = 3.0f;
+	public bool repeatable = false;
+
+	Transform currentView;
+	private Transform cameraPoint;
+
+	private GameObject camera;
+
+	void Start(){
+		camera = Camera.main.gameObject;
+		cameraPoint = gameObject.transform.Find ("Cam Point").transform;
+		camera.transform.position = cameraPoint.position;
+		currentAngle = camera.transform.rotation.eulerAngles;
+		//camera.transform.eulerAngles = cameraPoint.rotation.eulerAngles;
+	}
+
+
+
+	void OnTriggerEnter (Collider other) {
+		if (other.gameObject.tag.Equals ("Player")) {
+			currentAngle = camera.transform.rotation.eulerAngles;
+			currentView = cameraPoint;
+			startAngle = currentAngle;
+			endAngle = cameraPoint.transform.rotation.eulerAngles;
+			StartCoroutine (MoveCamera1 (camera.transform.position, cameraPoint.position, 2.0f));
+
+
+		}
+		//Debug.Log ("test");	
+	}
+
+	IEnumerator MoveCamera1(Vector3 startPos, Vector3 endPos, float time){
+		float i = 0.0f;
+		float rate = (1.0f / time) * transitionSpeed;
+		while (i < 1.0f) {
+			//Debug.Log ("Test " + i);
+			i += Time.deltaTime * rate;
+			camera.transform.position = Vector3.Lerp(startPos, endPos, i);
+			camera.transform.eulerAngles = Vector3.Lerp(startAngle,endAngle , i);
+			yield return null;
+		}
+
+	}
+
+
+}
+
---
+++ Scripts_van_Wessel/CameraSwitch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour {
+	public Camera cam1;
+	public Camera cam2;
+
+	// Use this for initialization
+	void Start () {
+		cam1.enabled = true;
+		cam2.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter (Collider other) {
+		if (other.gameObject.tag.Equals ("Player")) {
+			cam1.enabled = !cam1.enabled;
+			cam2.enabled = !cam2.enabled;
+			Debug.Log ("pass");
+		}
+	}
+}
